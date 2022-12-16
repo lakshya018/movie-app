6f58a0d25bdd936662c827ccbba2875eae563c84
Lakshya Soni <69206138+lakshya018@users.noreply.github.com>
2022-12-16 12:25:25 +0530

    Add files via upload

---
+++ src/comp/Home/Home.jsx
@@ -0,0 +1,59 @@
+import React, { useContext, useEffect, useState } from 'react'
+import MovieContext from '../context/MovieContext'
+import MovieCard from '../MovieCard/MovieCard';
+import './Home.css'
+const Home = () => {
+
+    //Used Context for getting movies
+    const context = useContext(MovieContext);
+    const { movies } = context;
+
+    const [recentMovies, setRecentMovies] = useState([]);
+
+    let searchString = localStorage.getItem('movieToSearch');
+
+    //When the page will load first time it will store the recent movies list in the recentMovies array
+    useEffect(() => {
+        const getRecentMovies = async () => {
+            const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=6a055d1774d93369d539edc4ef7f9089');
+            const json = await response.json();
+            setRecentMovies(json.results);
+        }
+
+        getRecentMovies();
+
+    }, [])
+
+    return (
+
+
+        //If the length of the movies array and the search string is empty then return the now playing or recent movies list, 
+        //else if we search something and we got no result then return "No Data Found", 
+        //else if we search somethinh and we found the data then return that movies data list
+
+        <div className="container moviesList">
+            {
+                (movies.length === 0 && searchString === null) ?
+                    recentMovies.map((movie) => {
+                        return <MovieCard key={movie.id} movie={movie} />
+                    })
+                :
+                    (movies.length === 0 && searchString.length === 0) ?
+                        recentMovies.map((movie) => {
+                            return <MovieCard key={movie.id} movie={movie} />
+                        })
+                :
+                    (movies.length === 0 && searchString.length > 0) ?
+                        <div className='noResult'><h3>No Data Found</h3></div>
+                :
+                    movies.map((movie) => {
+                        return <MovieCard key={movie.id} movie={movie} />
+                    })
+            }
+        </div>
+
+
+    )
+}
+
+export default Home
---
+++ src/comp/Navbar/Navbar.jsx
@@ -0,0 +1,61 @@
+import React, { useContext, useEffect, useState } from 'react'
+import logo from '../../insynk logo.png';
+import MovieContext from '../context/MovieContext';
+import './Navbar.css';
+import Home from '../Home/Home';
+
+const Navbar = () => {
+    const context = useContext(MovieContext);
+    const { getMovies } = context;
+
+    const [searchString, setSearchString] = useState("");
+
+    const handleChange = (e) => {
+        setSearchString(e.target.value);
+        localStorage.setItem("movieToSearch", e.target.value);
+    }
+
+    useEffect(() => {
+        localStorage.removeItem("movieToSearch");
+    }, [])
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            if (searchString.length === 0) {
+                alert("Please enter a movie name!");
+                return;
+            }
+            getMovies(searchString);
+        }
+    }
+
+    const handleSearch = () => {
+        if(searchString.length===0){
+            alert("Please enter a movie name!");
+            return;
+        }
+        getMovies(searchString);
+    }
+
+    return (
+
+        // ---- Navbar -----
+        <div className='container'>
+            <nav className='navbar'>
+                <div className='logo'>
+                    <img src={logo} alt="" />
+                </div>
+
+                <div className='search'>
+                    <input type="search" className="search-bar" onChange={handleChange} onKeyDown={handleKeyPress} placeholder='Search and press Enter' />
+                </div>
+            </nav>
+
+            <hr />
+        </div>
+
+
+    )
+}
+
+export default Navbar
